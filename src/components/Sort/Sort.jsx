aaea6786d0d06552f741d9d721d846961fb88bdf
Mugiwara52 <mugiwara52@mail.ru>
2025-01-23 00:04:10 +0300

    sixth

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,29 +1,44 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSort } from "../../redux/slices/filterSlice";
 
-const sortList = [
-  {name: 'популярности (DESC)', sortProperty: 'rating'},
-  {name: 'популярности (ASC)', sortProperty: '-rating'},
-  {name: 'цене (DESC)', sortProperty: 'price'},
-  {name: 'цене (ASC)', sortProperty: '-price'},
-  {name: 'алфавиту (DESC)', sortProperty: 'title'},
-  {name: 'алфавиту (ASC)', sortProperty: '-title'},
+// eslint-disable-next-line react-refresh/only-export-components
+export const sortList = [
+  { name: "популярности (DESC)", sortProperty: "rating" },
+  { name: "популярности (ASC)", sortProperty: "-rating" },
+  { name: "цене (DESC)", sortProperty: "price" },
+  { name: "цене (ASC)", sortProperty: "-price" },
+  { name: "алфавиту (DESC)", sortProperty: "title" },
+  { name: "алфавиту (ASC)", sortProperty: "-title" },
 ];
 
 function Sort() {
   const dispatch = useDispatch();
-  const sort = useSelector(state => state.filter.sort)
+  const sort = useSelector((state) => state.filter.sort);
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false);
 
   const selectedItem = (obj) => {
-    dispatch(setSort(obj))
+    dispatch(setSort(obj));
     setOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside)
+    }
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
@@ -47,7 +62,9 @@ function Sort() {
               <li
                 key={i}
                 onClick={() => selectedItem(obj)}
-                className={sort.sortProperty === obj.sortProperty ? "active" : ""}
+                className={
+                  sort.sortProperty === obj.sortProperty ? "active" : ""
+                }
               >
                 {obj.name}
               </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,15 @@ import { useContext, useEffect, useState } from "react";
 import { Pagination } from "../components/Pagination/Pagination";
 import { SearchContext } from "../App";
 import { useDispatch, useSelector } from "react-redux";
-import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
+import {
+  setCategoryId,
+  setCurrentPage,
+} from "../redux/slices/filterSlice";
 import axios from "axios";
 
 function Home() {
   const dispatch = useDispatch();
+
   const categoryId = useSelector((state) => state.filter.categoryId);
   const sortType = useSelector((state) => state.filter.sort.sortProperty);
   const currentPage = useSelector(state => state.filter.currentPage)
@@ -23,8 +27,9 @@ function Home() {
     dispatch(setCategoryId(id));
   };
   const onChangePageCount = (number) => {
-    dispatch(setCurrentPage(number))
-  }
+    dispatch(setCurrentPage(number));
+  };
+
 
   useEffect(() => {
     setIsLoading(true);
@@ -34,15 +39,17 @@ function Home() {
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const search = searchValue ? `&search=${searchValue}` : "";
 
-    axios.get(
-      `https://678aaab2dd587da7ac2b07b5.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-    )
-    .then((responce) => {
-      setItems(responce.data);
-      setIsLoading(false)
-    })
+    axios
+      .get(
+        `https://678aaab2dd587da7ac2b07b5.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((responce) => {
+        setItems(responce.data);
+        setIsLoading(false);
+      });
     window.scrollTo(0, 0);
   }, [categoryId, sortType, searchValue, currentPage]);
+
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeleton = [...new Array(6)].map((_, index) => (
